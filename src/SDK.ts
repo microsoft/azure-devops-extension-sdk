5e3ef81ef6f458376c45afd075d3bef58fbcba04
Yong Ma <yongma@microsoft.com>
2022-04-08 22:01:42 +0800

    add more context

--- src/SDK.ts
+++ src/SDK.ts
@@ -135,6 +135,558 @@ export interface IExtensionContext {
     version: string;
 }
 
+/**
+* Model that can be used to customize the values sent to AppInsights via "trackPage"
+*/
+export interface AppInsightsCustomTrackPageData {
+    alias: string;
+    metrics: {
+        [key: string]: any;
+    };
+    pageName: string;
+    properties: {
+        [key: string]: string;
+    };
+}
+
+/**
+* Model used to configure how TFS reports usage data to Application Insights
+*/
+export interface AppInsightsConfiguration {
+    /**
+    * If true, automatically call "trackPage" when the page is loaded
+    */
+    autoTrackPage: boolean;
+    /**
+    * Optional data used to override the default values sent to trackPage
+    */
+    customTrackPageData: AppInsightsCustomTrackPageData;
+    /**
+    * Set to false if app insights reporting is not enabled/configured
+    */
+    enabled: boolean;
+    /**
+    * The url from which to retrieve app insights scripts
+    */
+    insightsScriptUrl: string;
+    /**
+    * The instrumentation key used to track this deployment's usage
+    */
+    instrumentationKey: string;
+    /**
+    * If true, include collection, project, and team info in the track-page urls
+    */
+    trackProjectInfo: boolean;
+}
+
+/**
+* Reference to a javascript file to include on a page
+*/
+export interface JavascriptFileReference {
+    /**
+    * Condition to check in the case that Url lives on a CDN. The fallback script will be included if this check fails.
+    */
+    fallbackCondition: string;
+    /**
+    * Fallback url to use in case Url lives on a CDN
+    */
+    fallbackUrl: string;
+    /**
+    * Id of the reference (JQuery, JQueryUI, MicrosoftAjax, etc.)
+    */
+    identifier: string;
+    /**
+    * Is this a core javascript file that needs to be included in all child extension frames
+    */
+    isCoreModule: boolean;
+    /**
+    * Url of the javascript reference
+    */
+    url: string;
+}
+
+/**
+* Reference to a CSS file to include on a page
+*/
+export interface StylesheetReference {
+    /**
+    * Url of the high-contrast version of the CSS file
+    */
+    highContrastUrl: string;
+    /**
+    * Is this a core stylesheet that needs to be included in child frames
+    */
+    isCoreStylesheet: boolean;
+    /**
+    * Url of the CSS file
+    */
+    url: string;
+}
+
+/**
+* Contains lists of script and css references that need to be included on the page in order for the controls used by the page to work.
+*/
+export interface CoreReferencesContext {
+    /**
+    * Core 3rd party javascript bundle reference
+    */
+    coreScriptsBundle: JavascriptFileReference;
+    /**
+    * Core VSS javascript bundle reference for extension frames
+    */
+    extensionCoreReferences: JavascriptFileReference;
+    /**
+    * Core javascript files referenced on a page
+    */
+    scripts: JavascriptFileReference[];
+    /**
+    * Core CSS files referenced on a page
+    */
+    stylesheets: StylesheetReference[];
+}
+
+export interface DiagnosticsContext {
+    /**
+    * Id of the current activity
+    */
+    activityId: string;
+    allowStatsCollection: boolean;
+    /**
+    * Whether or not to enable static content bundling. This is on by default but the value can be overridden with a TFS-BUNDLING cookie or registry entry.
+    */
+    bundlingEnabled: boolean;
+    /**
+    * True if the CDN feature flag is enabled.
+    */
+    cdnAvailable: boolean;
+    /**
+    * True if the CDN feature flag is enabled and the user has not disabled CDN with a cookie.
+    */
+    cdnEnabled: boolean;
+    clientLogLevel: number;
+    debugMode: boolean;
+    /**
+    * Whether or not to diagnose the bundles.
+    */
+    diagnoseBundles: boolean;
+    inExtensionFallbackMode: boolean;
+    isDevFabric: boolean;
+    serviceVersion: string;
+    sessionId: string;
+    tracePointCollectionEnabled: boolean;
+    tracePointProfileEnd: string;
+    tracePointProfileStart: string;
+    /**
+    * Denotes the version of the web platform consumed by this service. Of the form M###.
+    */
+    webPlatformVersion: string;
+}
+
+export interface FeatureAvailabilityContext {
+    featureStates: {
+        [key: string]: boolean;
+    };
+}
+
+export interface GlobalizationContext {
+    culture: string;
+    /**
+    * Gets the explicitly-set theme, or the empty string if a theme was not explicitly set. An explicitly-set theme is set either in the query string (?theme=[themename]) or in the user's profile. However, the default theme set in the profile is not considered to be an explicitly-set theme.
+    */
+    explicitTheme: string;
+    theme: string;
+    timeZoneId: string;
+    timezoneOffset: number;
+    typeAheadDisabled: boolean;
+}
+
+/**
+* Model representing a hub in VSTS pages' navigation menu
+*/
+export interface Hub {
+    ariaLabel: string;
+    builtIn: boolean;
+    groupId: string;
+    hidden: boolean;
+    icon: string;
+    id: string;
+    isSelected: boolean;
+    name: string;
+    order: any;
+    supportsXHRNavigate: boolean;
+    uri: string;
+}
+
+/**
+* Model representing a hub group in VSTS pages' navigation menu
+*/
+export interface HubGroup {
+    builtIn: boolean;
+    hasHubs: boolean;
+    hidden: boolean;
+    icon: string;
+    id: string;
+    name: string;
+    nonCollapsible: boolean;
+    order: any;
+    uri: string;
+}
+
+export interface PinningPreferences {
+    pinnedHubGroupIds: string[];
+    pinnedHubs: {
+        [key: string]: string[];
+    };
+    unpinnedHubGroupIds: string[];
+    unpinnedHubs: {
+        [key: string]: string[];
+    };
+}
+
+/**
+* Context information containing the relevant hubs and hub groups for a given context
+*/
+export interface HubsContext {
+    allHubs: Hub[];
+    hubGroups: HubGroup[];
+    hubGroupsCollectionContributionId: string;
+    hubs: Hub[];
+    pinningPreferences: PinningPreferences;
+    selectedHubGroupId: string;
+    selectedHubId: string;
+    selectedNavigationIds: string[];
+}
+
+interface MicrosoftAjaxConfig {
+    cultureInfo: any;
+}
+
+/**
+* Flags to show which tokens of the navigation context are present in the current request URL. The request url's context part are formed like http://server:port[/{collection}[/{project}[/{team}]]][/_admin]/_{controller}/{action} The tokens {collection}, {project} and {team} are navigation level tokens whereas _admin segment is a switch to show admin areas of the site.
+*/
+declare enum NavigationContextLevels {
+    None = 0,
+    /**
+    * Root level in Azure.
+    */
+    Deployment = 1,
+    /**
+    * Root level in on premises. Neither of {collection}, {project} and {team} tokens have information
+    */
+    Application = 2,
+    /**
+    * Flag to show {collection} token has information.
+    */
+    Collection = 4,
+    /**
+    * Flag to show {project} token has information.
+    */
+    Project = 8,
+    /**
+    * Flag to show {team} token has information.
+    */
+    Team = 16,
+    /**
+    * Sugar for all application levels.
+    */
+    ApplicationAll = 30,
+    /**
+    * Sugar for all levels
+    */
+    All = 31,
+}
+
+/**
+* Structure to specify current navigation context of the executing request. The navigation context content's are generally obtained from the request URL. Some context specifiers such as "Account" can be implicit and might come from current IVssServiceHost.
+*/
+interface NavigationContext {
+    /**
+    * A token to show which area the request has been targeted to. By default there are two areas "Admin" and "Api". They can be specified in the URL as _admin and _api respectively.
+    */
+    area: string;
+    /**
+    * Command name for the current request's route. Used in telemetry and reporting.
+    */
+    commandName: string;
+    /**
+    * Current action route value
+    */
+    currentAction: string;
+    /**
+    * Current controller route value
+    */
+    currentController: string;
+    /**
+    * Current parameters route value (the path after the controller and action in the url)
+    */
+    currentParameters: string;
+    /**
+    * The id of the matched route
+    */
+    routeId: string;
+    /**
+    * The templates for the matched route
+    */
+    routeTemplates: string[];
+    /**
+    * The set of route values for this request
+    */
+    routeValues: { [key: string]: string; };
+    /**
+    * Flag to show top most navigation context. For example the URL http://server:port/collection/project/_controller/action sets the Project bit while the URL http://server:port/collection/project/_admin/_controller/action sets also sets the area property to Admin.
+    */
+    topMostLevel: NavigationContextLevels;
+}
+
+/**
+* Holds a lookup of urls for different services (at different host levels)
+*/
+interface ServiceLocations {
+    locations: { [key: string]: { [key: number]: string; }; };
+}
+
+interface DaylightSavingsAdjustmentEntry {
+    /**
+    * Millisecond adjustment from UTC
+    */
+    offset: number;
+    /**
+    * Date that the offset adjustment starts
+    */
+    start: Date;
+}
+
+interface TimeZonesConfiguration {
+    daylightSavingsAdjustments: DaylightSavingsAdjustmentEntry[];
+}
+
+/**
+* MVC api configuration
+*/
+interface ConfigurationContextApis {
+    /**
+    * Specifies the path prefix for the area
+    */
+    areaPrefix: string;
+    /**
+    * Specifies the path prefix for the controller
+    */
+    controllerPrefix: string;
+    /**
+    * Api-version for legacy rpc-style web access api controllers See WebApiVersionClient for the version coming from the client/browser.  The return value is a positive whole number >= 1.
+    */
+    webApiVersion: string;
+}
+
+interface TfsMailSettings {
+    enabled: boolean;
+}
+
+/**
+* Paths to server resources
+*/
+export interface ConfigurationContextPaths {
+    /**
+    * Path (no CDN) to versioned static content
+    */
+    cdnFallbackStaticRootTfs: string;
+    /**
+    * Relative path to the _content path of the web application
+    */
+    resourcesPath: string;
+    /**
+    * Relative path to the root of the web application
+    */
+    rootPath: string;
+    /**
+    * Absolute path to build static content URLs from. May be relative or fully-qualified.
+    */
+    staticContentRootPath: string;
+    /**
+    * Static content version stamp
+    */
+    staticContentVersion: string;
+    /**
+    * Relative path to unversioned 3rd party static content
+    */
+    staticRoot3rdParty: string;
+    /**
+    * Relative path to versioned static content
+    */
+    staticRootTfs: string;
+}
+
+/**
+* Web Access configuration data. This information is used to process requests on the server.  This data is also placed in a json island on each page in order for JavaScript to know key configuration data required to things like construct proper urls
+*/
+interface ConfigurationContext {
+    /**
+    * MVC api configuration
+    */
+    api: ConfigurationContextApis;
+    /**
+    * Optional name of the client (e.g. TEE) hosting the page
+    */
+    clientHost: string;
+    isHosted: boolean;
+    /**
+    * Current mail settings for TFS
+    */
+    mailSettings: TfsMailSettings;
+    /**
+    * Server resource paths
+    */
+    paths: ConfigurationContextPaths;
+    /**
+    * Indicates what URL format to use.
+    */
+    useCodexDomainUrls: boolean;
+}
+
+/**
+* Global context placed on each VSSF web page (through json island data) which gives enough information for core TypeScript modules/controls on the page to operate
+*/
+export interface IPageContext {
+    /**
+    * Configuration for reporting telemetry/usage data to App Insights
+    */
+    appInsightsConfiguration: AppInsightsConfiguration;
+    /**
+    * Core javascript and css references
+    */
+    coreReferences: CoreReferencesContext;
+    /**
+    * Specifies the prefixes for CSS modules that should map to the current service. e.g. "VSS/LoaderPlugins/Css!EMS:ExtensionManagement" would map to ExtensionManagement.css under the themed content path of this service if "EMS" is in the CSSModulePrefixes list.
+    */
+    cssModulePrefixes: string[];
+    /**
+    * Diagnostic related information for the current page
+    */
+    diagnostics: DiagnosticsContext;
+    /**
+    * Feature flag states to include by default in page data (avoids AJAX lookup)
+    */
+    featureAvailability: FeatureAvailabilityContext;
+    /**
+    * Globalization data for the current page based on the current user's settings
+    */
+    globalization: GlobalizationContext;
+    /**
+    * Cached set of hubs and hub groups for the given request/navigation-context
+    */
+    hubsContext: HubsContext;
+    /**
+    * Configuration needed for Microsoft.Ajax library
+    */
+    microsoftAjaxConfig: MicrosoftAjaxConfig;
+    /**
+    * Current navigation context.
+    */
+    navigation: NavigationContext;
+    /**
+    * The service instance type id for the VSTS service serving this page
+    */
+    serviceInstanceId: string;
+    serviceLocations: ServiceLocations;
+    /**
+    * Contains global time zone configuration information (e.g. which dates DST changes)
+    */
+    timeZonesConfiguration: TimeZonesConfiguration;
+    /**
+    * Web Access configuration
+    */
+    webAccessConfiguration: ConfigurationContext;
+    /**
+    * The web context information for the given page request
+    */
+    webContext: IWebContext;
+}
+
+export enum ContextHostType {
+    Unknown = 0,
+    /**
+    * The Deployment Host
+    */
+    Deployment = 1,
+    /**
+    * A legacy name for the Organization host. Use ContextHostType.Organization instead.
+    */
+    Application = 2,
+    /**
+    * The Organization host
+    */
+    Organization = 2,
+    /**
+    * The Project Collection
+    */
+    ProjectCollection = 4
+}
+
+export interface HostContext {
+    id: string;
+    name: string;
+    relativeUri: string;
+    uri: string;
+}
+
+export interface ExtendedHostContext {
+    authority: string;
+    hostType: ContextHostType;
+    id: string;
+    isAADAccount: boolean;
+    name: string;
+    relativeUri: string;
+    scheme: string;
+    uri: string;
+}
+
+export interface ContextIdentifier {
+    id: string;
+    name: string;
+}
+
+export interface TeamContext {
+    id: string;
+    name: string;
+}
+
+export interface UserContext {
+    email: string;
+    id: string;
+    limitedAccess: boolean;
+    name: string;
+    subjectId: string;
+    subjectType: string;
+    uniqueName: string;
+}
+
+/**
+* Context information for all web access requests
+*/
+interface IWebContext {
+    account: HostContext;
+    /**
+    * Information about the Collection used in the current request (may be null)
+    */
+    collection: HostContext;
+    /**
+    * Information about the current request context's host
+    */
+    host: ExtendedHostContext;
+    /**
+    * Information about the project used in the current request (may be null)
+    */
+    project: ContextIdentifier;
+    /**
+    * Information about the team used in the current request (may be null)
+    */
+    team: TeamContext;
+    /**
+    * Information about the current user
+    */
+    user: UserContext;
+}
+
 interface IExtensionHandshakeOptions extends IExtensionInitOptions {
 
     /**
@@ -144,6 +696,7 @@ interface IExtensionHandshakeOptions extends IExtensionInitOptions {
 }
 
 interface IExtensionHandshakeResult {
+    pageContext: IPageContext;
     contributionId: string;
     context: {
         extension: IExtensionContext,
@@ -151,13 +704,15 @@ interface IExtensionHandshakeResult {
         host: IHostContext
     },
     initialConfig?: { [key: string]: any };
-    themeData?: { [ key: string]: string };
+    themeData?: { [key: string]: string };
 }
 
 const hostControlId = "DevOps.HostControl";
 const serviceManagerId = "DevOps.ServiceManager";
 const parentChannel = channelManager.addChannel(window.parent);
 
+let webContext: IWebContext | undefined;;
+let hostPageContext: IPageContext | undefined;
 let extensionContext: IExtensionContext | undefined;
 let initialConfiguration: { [key: string]: any } | undefined;
 let initialContributionId: string | undefined;
@@ -204,6 +759,8 @@ export function init(options?: IExtensionInitOptions): Promise<void> {
         const initOptions = { ...options, sdkVersion };
 
         parentChannel.invokeRemoteMethod<IExtensionHandshakeResult>("initialHandshake", hostControlId, [initOptions]).then((handshakeData) => {
+            hostPageContext = handshakeData.pageContext;
+            webContext = hostPageContext.webContext;
 
             initialConfiguration = handshakeData.initialConfig || {};
             initialContributionId = handshakeData.contributionId;
@@ -304,6 +861,26 @@ export function getExtensionContext(): IExtensionContext {
 }
 
 /**
+* Get the context about the host page
+*/
+export function getPageContext(): IPageContext {
+    if (!hostPageContext) {
+        throw new Error(getWaitForReadyError("getExtensionContext"));
+    }
+    return hostPageContext;
+}
+
+/**
+* Get the context about the web
+*/
+export function getWebContext(): IWebContext {
+    if (!webContext) {
+        throw new Error(getWaitForReadyError("getExtensionContext"));
+    }
+    return webContext;
+}
+
+/**
 * Get the contribution with the given contribution id. The returned contribution has a method to get a registered object within that contribution.
 *
 * @param contributionId - Id of the contribution to get
@@ -358,7 +935,7 @@ export function resize(width?: number, height?: number): void {
     if (body) {
         const newWidth = typeof width === "number" ? width : (body ? body.scrollWidth : undefined);
         const newHeight = typeof height === "number" ? height : (body ? body.scrollHeight : undefined);
-        
+
         parentChannel.invokeRemoteMethod("resize", hostControlId, [newWidth, newHeight]);
     }
 }
