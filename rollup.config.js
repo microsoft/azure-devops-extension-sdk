03671c9b1482aa892d9548bb3d66d8708553813b
TFS MiniLab Admin <tfsmladm@microsoft.com>
2023-09-29 01:30:11 -0700

    Adding tsconfig for amd and esm

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+/**
+ * Rollup configuration file for building a JavaScript SDK in different formats: ESM and AMD
+ */
+
+import typescript from "@rollup/plugin-typescript"
+import del from "rollup-plugin-delete"
+import terser from "@rollup/plugin-terser"
